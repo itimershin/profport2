b244ca8898e9aa55ab5343f715a010e2a88627dd
itimershin <timershin.ildar@gmail.com>
2017-03-06 12:36:20 +0500

    del wrong

--- app/controllers/my_script.rb
+++ app/controllers/my_script.rb
@@ -5,6 +5,7 @@ end
 p generate_password(7)
 
 
+=begin
 person = { age: 15, name: 'Misha'}
 person.each { |k, v| p "#{k}" => "#{v}"}
 info = { age: 20, wage: 25000 }
@@ -14,4 +15,4 @@ hash = Hash.new(0)
 p hash[:id]
 
 arr = ["sfsdfsdf", "wqewer", "wqeqwe" ,"qweasad", "qweasad", "qweasad"]
-fre
+fre=end
